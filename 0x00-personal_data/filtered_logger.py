d53ec480ba3505a14b3608a73802006c34c627b6
Jojo <jojothomas1515@gmail.com>
2023-08-31 22:58:51 +0100

    logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,11 +4,7 @@ from typing import List
 import re
 
 
-def filter_datum(
-        fields: List[str],
-        redaction: str,
-        message: str,
-        seperator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str, seperator: str) -> str:
     for field in fields:
         pattern = r"(?<={}=)[^{}]*".format(field, seperator)
         message = re.sub(pattern, redaction, message)
