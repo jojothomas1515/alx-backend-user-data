ae09a3b2bf87270882cee1ff3972cb8cf9440f60
Jojo <jojothomas1515@gmail.com>
2023-08-31 22:57:51 +0100

    logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,20 +9,7 @@ def filter_datum(
         redaction: str,
         message: str,
         seperator: str) -> str:
-    """Filter datum.
-
-    Hides data on fields passed.
-
-    Args:
-        fields: The fields to obfuscate.
-        redaction: the text to mask the fields with.
-        message: the string to be processed and return.
-        seperator: seperater to used on the text
-    Return:
-          processed string
-    """
-    res = message
     for field in fields:
         pattern = r"(?<={}=)[^{}]*".format(field, seperator)
-        res = re.sub(pattern, redaction, res)
-    return res
+        message = re.sub(pattern, redaction, message)
+    return message
