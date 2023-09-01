f09c4cde43f75db919ea5a61c8f277ab865f9f88
Jojo <jojothomas1515@gmail.com>
2023-09-01 11:03:55 +0100

    Don

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,8 @@ from typing import List
 import re
 
 
-def filter_datum(fields, redaction, message, seperator):
+def filter_datum(fields: List[str],
+                 redaction: str, message: str, seperator: str) -> List[str]:
     """THis si a documentation."""
     for field in fields:
         message = re.sub(r"(?<={}=)[^{}]*".format(field, seperator),
