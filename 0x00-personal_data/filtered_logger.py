61b9b2e742c483679f66775818f5bdb6b2d7c2a4
Jojo <jojothomas1515@gmail.com>
2023-09-03 20:37:59 +0100

    RedactingFormatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -30,6 +30,10 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]) -> None:
+        """Constructor method.
+
+        :arg fields: list for fields to redact.
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
