ac92ce5033f73740090192737338f57645fe0f22
Jojo <jojothomas1515@gmail.com>
2023-09-04 18:29:17 +0100

    encrypting password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""Encrypting password."""
+from bcrypt import hashpw, gensalt
+
+
+def hash_password(password: str) -> bytes:
+    """Encrypt string with a salted hash.
+
+    Args:
+        password: the string to encrypt
+    Return: Salted hashed bytes string
+    """
+    return hashpw(password.encode(), gensalt(12))
