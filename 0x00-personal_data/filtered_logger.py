e5880c5470b55d26a2cceccc02325d08961867f3
Jojo <jojothomas1515@gmail.com>
2023-09-04 18:11:15 +0100

    main function added

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -66,3 +66,34 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         host=host
     )
     return connection
+
+
+def main() -> None:
+    """Main function.
+
+    Obtain a database connection using get_db
+    and retrieve all rows in the users table
+    and display each row under a filtered format
+    Filtered fields:
+        name
+        email
+        phone
+        ssn
+        password
+    """
+    conn = get_db()
+    logger = get_logger()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users")
+    for item in cur.fetchall():
+        column_names = cur.column_names
+        res = zip(column_names, item)
+        res = [f"{c}={i}" for c, i in res]
+        res = "; ".join(res)
+        logger.info(res)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
