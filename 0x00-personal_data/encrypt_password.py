57cb0c8568dd5edb931351d5a13fa258833ad9b8
Jojo <jojothomas1515@gmail.com>
2023-09-04 18:38:18 +0100

    encrypting password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,16 @@ def hash_password(password: str) -> bytes:
     Return: Salted hashed bytes string
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if the password matches the hash password.
+
+    Args:
+        hashed_password: hashed_password
+        password: the password to check it validity
+    Return:
+        True: if they match
+        False: if they don't match
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
