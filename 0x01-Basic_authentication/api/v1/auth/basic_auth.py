5ba5bb8903c96cff174cbe43e630d05a510d47da
Jojo <jojothomas1515@gmail.com>
2023-09-05 21:42:38 +0100

    auth check before request with basic_auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
 """Basic auth module."""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """Basic authentication class."""
 
-    pass
+    def extract_base64_authorization_header(
+        self,
+        authorization_header: str
+    ) -> str:
+        """Extract base64 authorization header."""
+
+        if authorization_header is None:
+            return None
+        elif type(authorization_header) != str:
+            return None
+        elif not authorization_header.startswith("Basic "):
+            return None
+
+        res = authorization_header.split(" ", 1)
+        if len(res) == 2:
+            return base64.b64encode(res[1].encode())
+        return None
