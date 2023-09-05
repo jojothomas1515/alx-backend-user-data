00553c22de8bee0454a28c1c0c4c4c1a26436271
Jojo <jojothomas1515@gmail.com>
2023-09-05 16:03:36 +0100

    unauthorized handler and route

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/api/v1/unauthorized', strict_slashes=False)
+@app_views.route('/unauthorized', strict_slashes=False)
 def unauth() -> None:
     """Route abort with an error code of 401."""
     abort(401)
