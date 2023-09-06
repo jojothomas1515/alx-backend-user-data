07a4fc8b0fd988ef70739633656ab81dcdf2f787
Jojo <jojothomas1515@gmail.com>
2023-09-06 14:01:06 +0100

    improved require auth method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -23,13 +23,19 @@ class Auth:
         elif len(excluded_paths) == 0:
             return True
 
+        # if the path ends with a "/" removes it
         if path.endswith("/"):
             path = path[:-1]
 
+        # compile regext that wiill match the path with excluded path that
+        # may or may not have "/" as long as the provious text match
         slash_match = re.compile(r"{}/?$".format(path))
         for p in excluded_paths:
-            glob_match = re.compile(r".*\*$")
-            if re.match(glob_match, p):
+
+            # if path  in the excluded path endswith an "*"
+            # compile a regex for the path excluding the "*"
+            # the matches any path that that has the excluded path in the str
+            if p.endswith("*"):
                 n_p = re.compile(r"{}\w*".format(p[:-1]))
                 if re.match(n_p, path):
                     return False
