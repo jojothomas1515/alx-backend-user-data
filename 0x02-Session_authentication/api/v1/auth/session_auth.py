8e8facfe72029d319962eb570806bdc2e5ee5150
Jojo <jojothomas1515@gmail.com>
2023-09-07 12:34:42 +0100

    user_id_for_session_id method implemented

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -24,3 +24,15 @@ class SessionAuth(Auth):
 
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Get user_id associated with a session id.
+
+        Args:
+            session_id: session id
+        Return: user_id associated with session_id
+        """
+        if not session_id or type(session_id) != str:
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
