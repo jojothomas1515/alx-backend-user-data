44b647516cb47190ed0fb3e614a1b97ac68df557
Jojo <jojothomas1515@gmail.com>
2023-09-15 00:14:10 +0100

    valid_login method implementation.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -40,3 +40,12 @@ class Auth:
         except NoResultFound:
             hashed_pwd = _hash_password(password)
             return self._db.add_user(email, hashed_pwd)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Check if the login details is valid."""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(), user.hashed_password)
+        except NoResultFound:
+            return False
+        return False
