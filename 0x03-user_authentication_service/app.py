1304ef89a932694b1ccbdb3c7a607fb13144c927
Jojo <jojothomas1515@gmail.com>
2023-09-14 23:52:20 +0100

    app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Flask app module."""
+
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.get("/", strict_slashes=True)
+def index():
+    """Index route."""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
