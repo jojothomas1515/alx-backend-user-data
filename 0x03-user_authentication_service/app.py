cbb91d0b38736c1233d040f52aea3299ac71a2c5
Jojo <jojothomas1515@gmail.com>
2023-09-15 23:13:00 +0100

    reset password route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -69,5 +69,19 @@ def profile():
     abort(403)
 
 
+@app.route("/reset_password", strict_slashes=False, methods=["POST"])
+def get_reset_password_token():
+    """reset password route function."""
+    email = request.cookies.get("email")
+
+    if not email:
+        abort(403)
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
