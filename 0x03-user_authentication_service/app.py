7b3eded4f736236b4e0e0b43939b867822d24605
Jojo <jojothomas1515@gmail.com>
2023-09-15 23:32:53 +0100

    update password method

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -82,6 +82,21 @@ def get_reset_password_token():
     except ValueError:
         abort(403)
 
+@app.route("/reset_password", strict_slashes=False, methods=["PUT"])
+def get_reset_password_token():
+    """reset password route function."""
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+
+    if not email or not  reset_token or not new_password:
+        abort(403)
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
