687c1ab919435768772a7fc374bba8117ac8eb92
Jojo <jojothomas1515@gmail.com>
2023-09-15 18:59:10 +0100

    logout route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -46,8 +46,6 @@ def login():
 def logout():
     """Logout route function."""
     session_id = request.cookies.get("session_id")
-    if not session_id:
-        return redirect("/")
 
     user = AUTH.get_user_from_session_id(session_id)
     if user:
