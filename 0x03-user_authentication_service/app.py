1602f37c9f4772e6a6424a9e0d0aec7c6755cdfb
Jojo <jojothomas1515@gmail.com>
2023-09-16 15:42:19 +0100

    logout method updated

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -52,7 +52,7 @@ def logout():
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
-        return redirect("/", 200)
+        return redirect("/")
     abort(403)
 
 
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -4,17 +4,20 @@ Main file
 """
 import requests
 
+
 def register_user(email: str, password: str) -> None:
     """Register user"""
     payload = {'email': email, 'password': password}
     r = requests.post('http://localhost:5000/users', data=payload)
     assert r.status_code == 200
 
+
 def log_in_wrong_password(email: str, password: str) -> None:
     """Log in with wrong password"""
     payload = {'email': email, 'password': password}
     r = requests.post('http://localhost:5000/sessions', data=payload)
     assert r.status_code == 401
+
 
 def log_in(email: str, password: str) -> str:
     """Log in"""
